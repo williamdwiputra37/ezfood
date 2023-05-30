475a073ee5fec5fd086716fdb178b4319c907768
William Trijanto <111202113240@mhs.dinus.ac.id>
2023-05-30 08:38:55 +0700

    authsystem

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                2023 © EZFood
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                     by EZTeam
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
